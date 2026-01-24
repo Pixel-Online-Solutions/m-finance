342f68e11aa25499206795f5948920458af461c1
PartickPinace <minor.michal1@gmail.com>
2026-01-24 15:18:27 +0100

    astro.config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
+
+const base = process.env.BASE_PATH || '/';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://patrickpinace.github.io',
+  base: base,
   vite: {
-    plugins: [tailwindcss()],
-    preview: {
-      allowedHosts: true  // ← DODAJ TĘ LINIĘ
-    }
+    plugins: [tailwindcss()]
   }
 });
