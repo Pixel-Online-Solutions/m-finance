96dfa7c5a7474cd8d0ec18e569e45f79231d4590
PatrickPinace <125275296+PatrickPinace@users.noreply.github.com>
2026-01-24 15:29:14 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
-const base = process.env.BASE_PATH || '/';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://patrickpinace.github.io',
-  base: base,
+  base: '/m-finance/',  // ← TU USTAW NA SZTYWNO ŚCIEŻKĘ REPO
   vite: {
     plugins: [tailwindcss()]
   }
